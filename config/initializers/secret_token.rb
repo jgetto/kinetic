6c4cd9eea1c2450a75bde309b80c1c858b476409
Jacob Getto <jacob.getto@gmail.com>
2011-10-21 20:20:23 -0400

    added rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kinetic::Application.config.secret_token = 'b95bb192906f68cebdac4abcb7a38e62178f2afc267352625d5b499232355efb186e020b75bb526f1b16726447ef16cd7ffd39468ba8d6ddea820c99092c811d'
