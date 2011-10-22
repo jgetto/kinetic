f43c47494f34a437390804a0debdf096eef41f07
rkstedman <rkstedman@Rosencrantz.(none)>
2011-10-21 20:25:36 -0400

    changed homepage

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Kinetic::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "kinetic#new"
 
   # See how all your routes lay out with "rake routes"
 
